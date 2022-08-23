1595e24cfda75c6c1a0a061c55adbb0c024b5dc7
valdaarhun <icegambit91@gmail.com>
2022-08-24 01:49:49 +0530

    Next generation render (1/2)

--- game/grid/grid.cpp
+++ game/grid/grid.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <utility>
 #include <vector>
 
 #include "../utilities/utils.h"
@@ -33,14 +34,10 @@ void Game::Grid::setRenderer(SDL_Renderer *renderer){
 }
 
 void Game::Grid::setAlive(int x, int y){
-    getIndices(&x, &y);
-    printf("%d %d\n", x, y);
     m_cells[y][x] = ALIVE;
 }
 
 void Game::Grid::setDead(int x, int y){
-    getIndices(&x, &y);
-    printf("%d %d\n", x, y);
     m_cells[y][x] = DEAD;
 }
 
@@ -50,6 +47,8 @@ void Game::Grid::createCell(int x, int y){
     SDL_SetRenderDrawColor(m_renderer, SET_BYTE(WHITE >> 16), SET_BYTE(WHITE >> 8), SET_BYTE(WHITE), ALPHA);
     SDL_RenderFillRect(m_renderer, &rect);
     SDL_RenderPresent(m_renderer);
+    getIndices(&x, &y);
+    // printf("%d %d\n", x, y);
     setAlive(x, y);
 }
 
@@ -59,9 +58,58 @@ void Game::Grid::killCell(int x, int y){
     SDL_SetRenderDrawColor(m_renderer, SET_BYTE(BLACK >> 16), SET_BYTE(BLACK >> 8), SET_BYTE(BLACK), ALPHA);
     SDL_RenderFillRect(m_renderer, &rect);
     SDL_RenderPresent(m_renderer);
+    getIndices(&x, &y);
+    // printf("%d %d\n", x, y);
     setDead(x, y);
 }
 
-void Game::Grid::run(){
+int Game::Grid::countNeighbours(int y, int x){
+    int cnt = 0;
+    int r[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+    int c[] = {0, 1, 1, 1, 0, -1, -1, -1};
+    for (int i = 0 ; i < 8 ; i++){
+        int n_x = x + c[i], n_y = y + r[i];
+        if (n_x < 0 || n_x >= m_cols || n_y < 0 || n_y >= m_rows)
+            continue;
+        cnt += (m_cells[n_y][n_x] == ALIVE);
+    }
+    return cnt;
+}
 
+void Game::Grid::setToToggle(int r, int c){
+    m_coords_toggle.push({r, c});
+}
+
+void Game::Grid::render(){
+    while(!m_coords_toggle.empty()){
+        std::pair< int, int > coords = m_coords_toggle.top();
+        printf("(%d, %d)\n", coords.first, coords.second);
+        m_coords_toggle.pop();
+        int x = coords.first * PIXELS_PER_CELL + 1;
+        int y = coords.second * PIXELS_PER_CELL + 1;
+        if (m_cells[coords.first][coords.second] == ALIVE)
+            killCell(x, y);
+        else
+            createCell(x, y);
+    }
+}
+
+void Game::Grid::nextGen(){
+    /* Cells outside grid are dead unconditionally */
+    for(int i = 0 ; i < m_rows ; i++){
+        for(int j = 0 ; j < m_cols ; j++){
+            if (m_cells[i][j] == ALIVE){
+                int num_neighbours = countNeighbours(i, j);
+                // printf("(%d, %d): %d\n", i, j, num_neighbours);
+                if (num_neighbours < 2 || num_neighbours > 3)
+                    setToToggle(i, j);
+            }
+            else{
+                int num_neighbours = countNeighbours(i, j);
+                if (num_neighbours == 3)
+                    setToToggle(i, j);
+            }
+        }
+    }
+    render();
 }
--- game/grid/grid.h
+++ game/grid/grid.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <stack>
 #include <vector>
 
 #include "../utilities/utils.h"
@@ -17,18 +18,22 @@ namespace Game{
         const int m_rows = PIXELS_Y / PIXELS_PER_CELL;
         SDL_Renderer *m_renderer;
         std::vector< std::vector< Vitals > > m_cells;
+        std::stack< std::pair < int, int > > m_coords_toggle;
 
         void getIndices(int *, int *);
         void getCoords(int *, int *);
         void setRect(SDL_Rect *, int, int);
         void setAlive(int, int);
         void setDead(int, int);
+        int countNeighbours(int, int);
+        void setToToggle(int, int);
+        void render();
 
     public:
         Grid();
         void setRenderer(SDL_Renderer *);
         void createCell(int, int);
         void killCell(int, int);
-        void run();
+        void nextGen();
     };
 }
