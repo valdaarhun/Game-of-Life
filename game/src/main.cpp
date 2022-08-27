e7c64afd3c69eff32636f9c6f133485918a45601
valdaarhun <icegambit91@gmail.com>
2022-08-27 15:09:12 +0530

    Reimplement main loop and template

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,46 +1,20 @@
 #include <SDL2/SDL.h>
 
-#include "world/world.h"
-#include "grid/grid.h"
-
 int main(){
-    Game::World world(1001, 801);
-    world.drawGrid();
-
-    Game::Grid grid;
-    grid.setRenderer(world.getRenderer());
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("Game of Life", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 800, SDL_WINDOW_RESIZABLE);
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
 
     SDL_Event event;
-    bool quit = false, running = false;
-    Uint32 now = 0, prev = 0;
-
+    bool quit = false;
     while(!quit){
-        event.type = -1;
         SDL_PollEvent(&event);
-
-        switch(event.type){
+        switch (event.type){
         case SDL_QUIT:
             quit = true;
             break;
-        case SDL_MOUSEBUTTONDOWN:
-            int x, y;
-            SDL_GetMouseState(&x, &y);
-            if (event.button.button == SDL_BUTTON_LEFT)
-                grid.createCell(x, y);
-            else if (event.button.button == SDL_BUTTON_RIGHT)
-                grid.killCell(x, y);
-            break;
-        case SDL_KEYDOWN:
-            if (event.key.keysym.scancode == SDL_SCANCODE_RETURN)
-                running = !running;
-            break;
         default:
             break;
-        }
-
-        if (running && ((now = SDL_GetTicks()) - prev) > 1000){
-            prev = now;
-            grid.nextGen();
         }
     }
 }
